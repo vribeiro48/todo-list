1b91e63ce86e30268d1f5e3f832de4451486640b
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2022-09-29 23:33:50 -0300

    Implementing the done state for a task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,29 @@
 import styles from './Task.module.css'
 
 import { Trash } from 'phosphor-react'
+import { ChangeEvent, useState } from 'react';
 
 interface TaskProps {
   content: string;
 }
 
 export function Task(props: TaskProps) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleTaskDone(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.checked) {
+      setIsChecked(true)
+    }
+    else {
+      setIsChecked(false)
+    }
+  }
+
   return (
-    <li className={styles.task}>
+    <li className={`${styles.task} ${isChecked ? styles.taskDone : styles.taskToDo }`}>
       <div className={styles.inputTaskName}>
         <span>
-          <input type="checkbox" />
+          <input type="checkbox" onChange={handleTaskDone} checked={isChecked} />
         </span> 
         <span>
           {props.content}
