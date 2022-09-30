ab63a25e501e9af78432169e4738f3398eedae2b
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2022-09-29 21:55:24 -0300

    Removing CreateTask from App

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ export default function App() {
   return (
     <div>
       <Header />
-      <div className={styles.wrapper}>
-        <CreateTask />
+      <div className={styles.wrapper}>        
         <TaskList />
       </div>
     </div>
